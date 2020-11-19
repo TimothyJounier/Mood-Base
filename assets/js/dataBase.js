e65751064ca983bdd09e84b11146efc8370b2b1c
Timothy Jounier <timothyjounier@gmail.com>
2020-11-19 15:43:35 +0100

    Structure final

---
+++ assets/js/dataBase.js
@@ -0,0 +1,96 @@
+let arrayProduct = []
+let product = new Object;
+
+// Categorie charge base
+
+// Support 1
+product.name = "Wood Flow";
+product.ref = "r001 ";
+product.category = "Phone";
+product.price = "29.90";
+product.description = "Station de beau téléphone. Fait à la main à partir de bois d’orme recyclé magnifique. Ajouter une touche de nature à votre espace de travail ou à domicile avec une station de téléphone en bois naturel. ";
+product.img = "assets/img/Phone1";
+
+// Support 2
+product.name = "Wood Magic";
+product.ref = "r002";
+product.category = "Phone";
+product.price = "28.65";
+product.description = "Vous cherchez toujours vos câbles de recharge et ne savez jamais où mettre votre téléphone ? Voici la station Magic";
+product.img = "assets/img/Phone2";
+
+// Support 3
+product.name = "Wood Dock";
+product.ref = "r003";
+product.category = "Phone";
+product.price = "29.30";
+product.description = "Station en bois naturel est aussi solide construire pour une meilleure stabilisation et un look naturel pour la maison ou le bureau.";
+product.img = "assets/img/Phone3";
+
+// Support 4
+product.name = "Wood Basic";
+product.ref = "r004";
+product.category = "Phone";
+product.price = "27.90";
+product.description = "Chargez votre iPhone et Votre Apple Watch avec notre borne de recharge personnalisée en bambou écologique et personnalisée avec n’importe quel nom.";
+product.img = "assets/img/Phone4";
+
+// Categorie Support Enceinte base
+
+product.name = "Wood Dupstep";
+product.ref = "r101";
+product.category = "Enceinte";
+product.price = "79.90";
+product.description = "Objet unique fabriqué à la main par nos artisans, l’enceinte de téléphone mangobeat® saura trouver sa place dans votre intérieur.";
+product.img = "assets/img/Enceinte1";
+
+product.name = "Wood Electro";
+product.ref = "r102";
+product.category = "Enceinte";
+product.price = "76.85";
+product.description = "Envie de pouvoir écouter vos sons préférés partout tout le temps ? Voici l'enceinte Electro du siècle";
+product.img = "assets/img/Enceinte2";
+
+product.name = "Wood Zouk";
+product.ref = "r103";
+product.category = "Enceinte";
+product.price = "78.50";
+product.description = "Pratique, car adaptable à tous type de smartphones (Iphone, Samsung, Huawei, Sony etc…), elle amplifie le son sortant par 5 à 8 fois, et ce sans batterie.";
+product.img = "assets/img/Enceinte3";
+
+product.name = "Wood LofI";
+product.ref = "r104";
+product.category = "Enceinte";
+product.price = "80";
+product.description = "Travaillée à la main, notre enceinte naturelle sans fil mangobeat® se fondra parfaitement dans votre intérieur.";
+product.img = "assets/img/Enceinte4";
+
+// Categorie Support Bureautique
+
+product.name = "Wood Simple";
+product.ref = "r201";
+product.category = "Bureautique";
+product.price = "49.90";
+product.description = "Cette station d’accueil universelle a été conçue pour être compatible avec TOUS les smartphones et il fonctionne également les cas pour ces téléphones.";
+product.img = "assets/img/Bureautique1";
+
+product.name = "Wood Gun";
+product.ref = "r202";
+product.category = "Bureautique";
+product.price = "55.90";
+product.description = "La station d’accueil en bois est une excellente option cadeau pour les vacances et tous les autres événements spéciaux. Notre conception est unique et fonctionnelle. Il a deux pièces dont vous aurez besoin pour les insérer les uns dans les autres. <";
+product.img = "assets/img/Bureautique2";
+
+product.name = "Wood Cut";
+product.ref = "r203";
+product.category = "Bureautique";
+product.price = "48.90";
+product.description = "La station personnalisée d’accueil est un cadeau original original fait main pour les hommes et les femmes, père, frère de mère, soeur, grands-parents, petite amie et petit ami, parfait pour une table de chevet, commode ou table de fin.";
+product.img = "assets/img/Bureautique3";
+
+product.name = "Wood Briquet";
+product.ref = "r204";
+product.category = "Bureautique";
+product.price = "49.90";
+product.description = "Voici la station Super Briquet.Toutes les pièces sont soigneusement conçues, détaillées, poncées et peintes à la main avec des taches.";
+product.img = "assets/img/Bureautique4";
