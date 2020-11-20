968fa6483ea3e2c5675ef2cc20325238b5de99a6
Timothy Jounier <timothyjounier@gmail.com>
2020-11-20 15:10:01 +0100

    Fonction Card en cours

--- assets/js/dataBase.js
+++ assets/js/dataBase.js
@@ -2,43 +2,43 @@ let arrayProduct = [
     // Categorie charge base
 
     // Support 1
-    {name:"Wood Flow", ref: 'r001', category: 'Phone', price = "29.90", description = "Station de beau téléphone. Fait à la main à partir de bois d’orme recyclé magnifique. Ajouter une touche de nature à votre espace de travail ou à domicile avec une station de téléphone en bois naturel. ", img = "assets/img/Phone1"},
+    {name:"Wood Flow", ref: 'r001', category: 'Phone', price: "29.90", description: "Station de beau téléphone. Fait à la main à partir de bois d’orme recyclé magnifique. Ajouter une touche de nature à votre espace de travail ou à domicile avec une station de téléphone en bois naturel. ", img: "assets/img/Phone/Phone1.jpg"},
 
     // Support 2
-    {name: "Wood magic", ref: 'r002', category = "Phone", category = "Phone", price = "28.65", description = "Vous cherchez toujours vos câbles de recharge et ne savez jamais où mettre votre téléphone ? Voici la station Magic", img = "assets/img/Phone2"},
+    {name: "Wood magic", ref: 'r002', category: "Phone", category: "Phone", price: "28.65", description: "Vous cherchez toujours vos câbles de recharge et ne savez jamais où mettre votre téléphone ? Voici la station Magic", img : "assets/img/Phone/Phone2.jpg"},
 
     // Support 3
-    {name = "Wood Dock", ref = "r003", category = "Phone",  price = "29.30", description = "Station en bois naturel est aussi solide construire pour une meilleure stabilisation et un look naturel pour la maison ou le bureau.", img = "assets/img/Phone3"},
+    {name : "Wood Dock", ref: "r003", category: "Phone",  price: "29.30", description: "Station en bois naturel est aussi solide construire pour une meilleure stabilisation et un look naturel pour la maison ou le bureau.", img: "assets/img/Phone/Phone3.jpg"},
 
     // Support 4
-    {name = "Wood Basic",ref = "r004", category = "Phone", price = "27.90", description = "Chargez votre iPhone et Votre Apple Watch avec notre borne de recharge personnalisée en bambou écologique et personnalisée avec n’importe quel nom.", img = "assets/img/Phone4" },
+    {name: "Wood Basic",ref: "r004", category: "Phone", price: "27.90", description: "Chargez votre iPhone et Votre Apple Watch avec notre borne de recharge personnalisée en bambou écologique et personnalisée avec n’importe quel nom.", img: "assets/img/Phone/Phone4.jpg" },
 
     // Categorie Support Enceinte base
 
     // Enceinte 1
-    {name = "Wood Dupstep", ref = "r101", category = "Enceinte", price = "79.90", description = "Objet unique fabriqué à la main par nos artisans, l’enceinte de téléphone mangobeat® saura trouver sa place dans votre intérieur.", img = "assets/img/Enceinte1"},
+    {name: "Wood Dupstep", ref: "r101", category: "Enceinte", price: "79.90", description: "Objet unique fabriqué à la main par nos artisans, l’enceinte de téléphone mangobeat® saura trouver sa place dans votre intérieur.", img: "assets/img/Enceinte/Enceinte1.jpg"},
 
     // Enceinte 2
-    {name = "Wood Electro", ref = "r102", category = "Enceinte", price = "76.85", description = "Envie de pouvoir écouter vos sons préférés partout tout le temps ? Voici l'enceinte Electro du siècle", img = "assets/img/Enceinte2"},
+    {name: "Wood Electro", ref: "r102", category: "Enceinte", price: "76.85", description: "Envie de pouvoir écouter vos sons préférés partout tout le temps ? Voici l'enceinte Electro du siècle", img: "assets/img/Enceinte/Enceinte2.jpg"},
 
     // Enceinte 3
-    {name = "Wood Zouk", ref = "r103", category = "Enceinte", price = "78.50", description = "Pratique, car adaptable à tous type de smartphones (Iphone, Samsung, Huawei, Sony etc…), elle amplifie le son sortant par 5 à 8 fois, et ce sans batterie.", img = "assets/img/Enceinte3"},
+    {name: "Wood Zouk", ref: "r103", category: "Enceinte", price: "78.50", description: "Pratique, car adaptable à tous type de smartphones (Iphone, Samsung, Huawei, Sony etc…), elle amplifie le son sortant par 5 à 8 fois, et ce sans batterie.", img: "assets/img/Enceinte/Enceinte3.jpg"},
 
     // Enceinte 4
-    {name = "Wood LofI", ref = "r104", category = "Enceinte", price = "80", description = "Travaillée à la main, notre enceinte naturelle sans fil mangobeat® se fondra parfaitement dans votre intérieur.", img = "assets/img/Enceinte4"},
+    {name: "Wood LofI", ref: "r104", category: "Enceinte", price: "80", description: "Travaillée à la main, notre enceinte naturelle sans fil mangobeat® se fondra parfaitement dans votre intérieur.", img: "assets/img/Enceinte/Enceinte4.jpg"},
 
 
     // Categorie Support Bureautique
 
     // Bureau 1
-    {name = "Wood Simple", ref = "r201", category = "Bureautique", price = "49.90", description = "Cette station d’accueil universelle a été conçue pour être compatible avec TOUS les smartphones et il fonctionne également les cas pour ces téléphones.", img = "assets/img/Bureautique1"},
+    {name: "Wood Simple", ref: "r201", category: "Bureautique", price: "49.90", description: "Cette station d’accueil universelle a été conçue pour être compatible avec TOUS les smartphones et il fonctionne également les cas pour ces téléphones.", img: "assets/img/Bureautique/Bureautique1.jpg"},
 
     // Bureau 2
-    {name = "Wood Gun", ref = "r202", category = "Bureautique", price = "55.90", description = "La station d’accueil en bois est une excellente option cadeau pour les vacances et tous les autres événements spéciaux. Notre conception est unique et fonctionnelle. Il a deux pièces dont vous aurez besoin pour les insérer les uns dans les autres.", img = "assets/img/Bureautique2"},
+    {name: "Wood Gun", ref: "r202", category: "Bureautique", price: "55.90", description: "La station d’accueil en bois est une excellente option cadeau pour les vacances et tous les autres événements spéciaux. Notre conception est unique et fonctionnelle. Il a deux pièces dont vous aurez besoin pour les insérer les uns dans les autres.", img: "assets/img/Bureautique/Bureautique2.jpg"},
 
     // Bureau 3
-    {name = "Wood Cut", ref = "r203", category = "Bureautique", price = "48.90", description = "La station personnalisée d’accueil est un cadeau original original fait main pour les hommes et les femmes, père, frère de mère, soeur, grands-parents, petite amie et petit ami, parfait pour une table de chevet, commode ou table de fin.", img = "assets/img/Bureautique3"},
+    {name: "Wood Cut", ref: "r203", category: "Bureautique", price: "48.90", description: "La station personnalisée d’accueil est un cadeau original original fait main pour les hommes et les femmes, père, frère de mère, soeur, grands-parents, petite amie et petit ami, parfait pour une table de chevet, commode ou table de fin.", img: "assets/img/Bureautique/Bureautique3.jpg"},
 
     // Bureau 4
-    {name = "Wood Briquet", ref = "r204", category = "Bureautique", price = "49.90", description = "Voici la station Super Briquet.Toutes les pièces sont soigneusement conçues, détaillées, poncées et peintes à la main avec des taches.", img = "assets/img/Bureautique4"},
+    {name: "Wood Briquet", ref: "r204", category: "Bureautique", price: "49.90", description: "Voici la station Super Briquet.Toutes les pièces sont soigneusement conçues, détaillées, poncées et peintes à la main avec des taches.", img: "assets/img/Bureautique/Bureautique4.jpg"},
 ]
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+ let original = document.querySelector('#card-product');
+ let products = document.getElementById('products')
+
+function showProduct(element,index){
+    document.querySelector('#card-product .img-card').src = element.img
+    document.querySelector('#card-product .title-card').innerHTML = element.name
+    document.querySelector('#card-product .description-card').innerHTML = element.description
+    document.querySelector('#card-product .price-card').innerHTML = element.price
+    let clone = original.cloneNode(true); //"deep" clone
+    clone.id = 'card-product' + index;
+    products.appendChild(clone);
+}
+
+arrayProduct.forEach(showProduct)
+
