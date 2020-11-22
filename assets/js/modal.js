180e356a4d0ba372dba44eaac04ad2b9635404a2
Your Name <morel.florian80@hotmail.fr>
2020-11-22 10:43:20 +0100

    Ajout du css js html dans la master

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,24 +1,175 @@
-let panier = [
-    // Categorie charge base
+//Essaie 2 
+//Remplacer ici le .add-cart par l'id qui sera généré dans les cards 
+let carts = document.querySelectorAll('#card-product');
 
-    // Support 1
-    {name:"Wood Flow", ref: "r001", category: "Phone", price : "29.90", description : "Station de beau téléphone. Fait à la main à partir de bois d’orme recyclé magnifique. Ajouter une touche de nature à votre espace de travail ou à domicile avec une station de téléphone en bois naturel. ", img : "assets/img/Phone1"},
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(arrayProduct[0]);
+        totalCost(arrayProduct[i])
+    })
+}
+//fonction pour que le nombre de produit dans le panier reste à sa valeur enregistrée même après le refresh 
+onLoadCartNumbers = () => {
 
-    // Support 2
-    // {name: "Wood magic", ref: "r002", category : "Phone", category : "Phone", price : "28.65", description : "Vous cherchez toujours vos câbles de recharge et ne savez jamais où mettre votre téléphone ? Voici la station Magic", img : "assets/img/Phone2"},
-]
-let originalProduct = document.querySelector('cardModal');
+    //Lorqu'on lui passe le nom d'une clé, cette méthode renvoie la valeur de la clé correspondante.
+    let productNumbers = localStorage.getItem('cartNumbers');
 
-function showProductModal(element, index){
-    document.querySelector('#card-product .img-card').src = element.img
-    document.querySelector('#card-product .title-card').innerHTML = element.name
-    document.querySelector('#card-product .price-card').innerHTML = element.price
+    //.cart correspond à la class du panier dans le barre nav ***** la span correspond au 0 à côté du panier
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+cartNumbers = (product) => {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        //Le localStorage va permettre d'accéder à un objet local Storage et donc au stockage de session. Ca va permettrere d'ajouter, de modifier ou de supprimer des éléments enregistrés 
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        //La méthode setItem() de l'interface Storage, lorsque lui sont passées le duo clé-valeur, les ajoute à l'emplacement de stockage, sinon elle met à jour la valeur si la clé existe déjà. Execption pour la nav privée
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1)
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItem(product);
+}
+
+setItem = (product) => {
+        let cartItems = localStorage.getItem('productsInCart')
+        cartItems = JSON.parse(cartItems);
+
+
+        if (cartItems != null) {
+            if (cartItems[product.ref] == undefined) {
+                cartItems = {
+                    ...cartItems,
+                    [product.ref]: product
+                }
+            }
+            cartItems[product.ref].inCart += 1;
+        } else {
+            product.inCart = 1;
+            cartItems = {
+                [product.ref]: product
+            }
         }
-    
+        localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+    }
+    //fonction pour calculer le montant total de cart
+totalCost = (product) => {
+    // console.log('The product price is :', product.price);
+    let cartCost = localStorage.getItem('totalCost');
 
-        let productModal = document.querySelector('#shoppingModal .modal-body .cardModal').innerHTML = ref;
-        
-    
+
+    console.log('My cartCost is', cartCost);
+    console.log(typeof cartCost);
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItem('totalCost', product.price);
+    }
+}
+
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector('.productsCart');
+
+
+    console.log(cartItems);
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `<div class="product-title-cart">
+                 <ion-icon name="close-circle-outline"></ion-icon>
+                 <img src="./img/${item.ref}.jpg">
+                 <span>${item.name}</span></div>
+                 <div class="price-cart">${item.price}</div>
+                 <div class="quantity-cart">
+                 <ion-icon name="caret-back-circle-outline"></ion-icon>
+                 <span>${item.inCart}</span>
+                 <ion-icon name="caret-forward-circle-outline"></ion-icon>
+                 </div>
+                 <div class="total-cart">
+                 ${item.inCart*item.price}€
+                 </div>
+                 `;
+        });
+
+        productContainer.innerHTML +=
+            `
+ <div class="basketTotalContainer">
+ <h4 class="basketTotalTitle">
+ Total panier
+ </h4> 
+ <h4 class="basketTotal"> 
+ ${cartCost}€
+ </h4></div>
+ `;
+
+    }
+}
+onLoadCartNumbers();
+displayCart();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Essaie 1 
+
+// let panier = [
+
+// Support 1
+// {
+//     name: "Wood Flow",
+//     ref: "r001",
+//     category: "Phone",
+//     price: "29.90",
+//     description: "Station de beau téléphone. Fait à la main à partir de bois d’orme recyclé magnifique. Ajouter une touche de nature à votre espace de travail ou à domicile avec une station de téléphone en bois naturel. ",
+//     img: "assets/img/Phone1"
+// },
+
+// Support 2
+//     { name: "Wood magic", ref: "r002", category: "Phone", category: "Phone", price: "28.65", description: "Vous cherchez toujours vos câbles de recharge et ne savez jamais où mettre votre téléphone ? Voici la station Magic", img: "assets/img/Phone2" },
+// ]
+// let originalProduct = document.querySelector('cardModal');
+
+// function showProductModal(productsPanier) {
+//     document.querySelector('#card-product .img-card').src = element.img
+//     document.querySelector('#card-product .title-card').innerHTML = element.name;
+//     document.querySelector('#card-product .price-card').innerHTML = element.price;
+
+//     let clone = originalProduct.cloneNode(true); //"deep" clone
+//     clone.id = 'cardModal' + index;
+//     products.appendChild(clone);
+// }
+// arrayProduct.forEach(showProductModal);
+
+// let productModal = document.querySelector('#shoppingModal .modal-body .cardModal').innerHTML = ref;
+
+
 
 
 
