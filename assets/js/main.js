cb8a3c98d8904265ebfc39af41ba5db113127078
FlorianMorel80 <you@example.com>
2020-11-23 14:29:05 +0100

    Push de l'après midi

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function showProduct(element, index) {
     document.querySelector('#card-product .img-card').src = element.img
     document.querySelector('#card-product .title-card').innerHTML = element.name
     document.querySelector('#card-product .description-card').innerHTML = element.description
-    document.querySelector('#card-product .price-card').innerHTML = element.price
+    document.querySelector('#card-product .price-card').innerHTML = element.price + "€";
     let clone = original.cloneNode(true); //"deep" clone
 
     clone.id = 'card-product' + index;
